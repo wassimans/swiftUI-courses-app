80508342d370518574c424d1833347572d6c8495
Wassim MANSOURI <wassim.mansouri@redfabriq.com>
2021-02-21 07:17:36 +0100

    Initial commit

---
+++ Courses/Courses/CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesApp.swift
+//  Courses
+//
+//  Created by Wassim Mansouri on 21/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
