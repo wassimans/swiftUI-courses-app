446d4ca26bf537fd665384bc093359ee0bb9206c
Wassim MANSOURI <wassim.mansouri@redfabriq.com>
2021-02-22 12:37:44 +0100

    Add cards and apply initial animations

--- Courses/Courses/ContentView.swift
+++ Courses/Courses/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var show = false
+  @State var viewSate = CGSize.zero
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      ZStack {
+        TitleView()
+          .blur(radius: show ? 20 : 0)
+          .animation(.default)
+        
+        BackCardView()
+          .background(show ? Color("card3") : Color("card4"))
+          .cornerRadius(20)
+          .shadow(radius: 20)
+          .offset(x: 0, y: show ? -400 : -40)
+          .offset(x: viewSate.width, y: viewSate.height)
+          .scaleEffect(0.9)
+          .rotationEffect(Angle(degrees: show ? 0: 10))
+          .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
+          .blendMode(.hardLight)
+          .animation(.easeInOut(duration: 0.5))
+        
+        BackCardView()
+          .background(show ? Color("card4") : Color("card3"))
+          .cornerRadius(20)
+          .shadow(radius: 20)
+          .offset(x: 0, y: show ? -200 : -20)
+          .offset(x: viewSate.width, y: viewSate.height)
+          .scaleEffect(0.95)
+          .rotationEffect(Angle(degrees: show ? 0 : 5))
+          .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+          .blendMode(.hardLight)
+          .animation(.easeInOut(duration: 0.3))
+        
+        CardView()
+          .offset(x: viewSate.width, y: viewSate.height)
+          .blendMode(.hardLight)
+          .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+          .onTapGesture {
+            show.toggle()
+          }
+          .gesture(
+            DragGesture().onChanged({ (value) in
+              self.viewSate = value.translation
+              self.show = true
+            })
+            .onEnded({value in
+              self.viewSate = .zero
+              self.show = false
+            })
+          )
+        
+        BottomCardView()
+          .blur(radius: show ? 20 : 0)
+          .animation(.default)
+      }
     }
 }
 
@@ -18,4 +70,81 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct CardView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        VStack(alignment: .leading) {
+          Text("UI Design")
+            .foregroundColor(.white)
+            .font(.title)
+            .fontWeight(.semibold)
+          Text("Certificate")
+            .foregroundColor(Color("accent"))
+        }
+        Spacer()
+        Image("Logo1")
+      }
+      .padding(.horizontal, 20)
+      .padding(.top, 20)
+      Image("Card1")
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(width: 300, height: 110, alignment: .top)
+    }
+    .frame(width: 340, height: 220)
+    .background(Color.black)
+    .cornerRadius(20.0)
+    .shadow(radius: 20)
+  }
+}
+
+struct BackCardView: View {
+  var body: some View {
+    VStack {
+      Spacer()
+    }
+    .frame(width: 340, height: 220)
+  }
+}
+
+struct TitleView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Certificates")
+          .font(.largeTitle)
+          .fontWeight(.bold)
+        Spacer()
+      }
+      .padding()
+      Image("Background1")
+      Spacer()
+    }
+  }
+}
+
+struct BottomCardView: View {
+  var body: some View {
+    VStack(spacing: 20) {
+      Rectangle()
+        .frame(width: 40, height: 5)
+        .opacity(0.1)
+        .cornerRadius(3)
+      Text("This certificate is proof that Wassim Mansouri has achieved the UI design course with approval from Design+Code instructor.")
+        .multilineTextAlignment(.center)
+        .font(.subheadline)
+        .lineSpacing(4)
+      Spacer()
+    }
+    .padding(.top, 8)
+    .padding(.horizontal, 20)
+    .frame(maxWidth: .infinity)
+    .background(Color.white)
+    .cornerRadius(30)
+    .shadow(radius: 20)
+    .offset(x: 0, y: 550)
+  }
 }
